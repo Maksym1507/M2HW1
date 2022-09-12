3917e3dc7dac318950c39fb35dfde64c6361d6c0
Max <maksym.illarionov@nure.ua>
2022-09-12 14:22:47 +0300

    Was developed Logger which collects info about the state of the application.

---
+++ M2HW1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace M2HW1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
