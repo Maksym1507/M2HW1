270e84febaa19db0eeaf052375510572c9380145
Max <maksym.illarionov@nure.ua>
2022-09-12 14:22:47 +0300

    Was developed Logger which collects info about the state of the application.

---
+++ M2HW1/Actions.cs
@@ -0,0 +1,31 @@
+﻿namespace M2HW1
+{
+    public class Actions
+    {
+        static Actions()
+        {
+            Logger = Logger.Instance;
+        }
+
+        private static Logger Logger { get; }
+
+        public Result InfoMethod()
+        {
+            Logger.WriteLog(LogType.Info, $"Start method {nameof(InfoMethod)}");
+
+            return new () { Status = true };
+        }
+
+        public Result WarningMethod()
+        {
+            Logger.WriteLog(LogType.Warning, $"Skipped logic in method {nameof(WarningMethod)}");
+
+            return new () { Status = true };
+        }
+
+        public Result ErrorMethod()
+        {
+            return new () { ErrorMessage = "I broke a logic" };
+        }
+    }
+}
